451b51825e70bfffca4271880548ef91c7fa2f86
mzzhref <mzzhref@163.com>
2018-08-20 09:41:46 +0800

    第一次提交代码

---
+++ js/player.js
@@ -0,0 +1,208 @@
+/*
+ * @Author: Mr.Miao
+ * @Date:   2018-08-19 17:04:51
+ * @Last Modified by:   Mr.Miao
+ * @Last Modified time: 2018-08-20 01:17:12
+ */
+$(function() {
+    // 音乐播放器
+    var $audio = document.getElementById("audio");
+    // 播放开关
+    var audioBtn = true;
+    // 定时器
+    var iTimer = null;
+    // 播放当前时间
+    var currentTime = null;
+    // 总时长
+    var duration = null;
+    // 音量
+    var volume = null;
+    // 音量是否静音
+    var volumeBtn = true;
+    // 默认音量
+    $audio.volume = 0;
+    volume = 0;
+    $("#volume-jd-width").css({ width: 0 })
+    // 开始或暂停
+    var mBtn = $("#play");
+    // 上一首
+    var prev = $("#prev");
+    // 下一首
+    var next = $("#next");
+    // 播放当前时间
+    var currentTimeTxt = $("#currentTime");
+    var jinduDiv = $("#jindu");
+    // 总时长
+    var durationTxt = $("#duration");
+    // 音量
+    var volumeA = $("#volume");
+    var volumeDiv = $("#volume-jd");
+
+    var num = 0;
+    var arr = [1, 2, 3];
+
+    var $music = {
+        init() {
+            $music.m_play();
+            $music.m_click();
+            $music.m_prev_click();
+            $music.m_next_click();
+            $music.m_volume();
+            $music.m_time_jd();
+            $music.m_volume_jd();
+        },
+        m_time() {
+            clearInterval(iTimer);
+            iTimer = setInterval(function() {
+                // 记录时间
+                duration = $audio.duration;
+                currentTime = $audio.currentTime;
+                console.log(duration)
+                console.log(currentTime)
+                // 写入时间
+                durationTxt.html(psn_time($audio.duration));
+                currentTimeTxt.html(psn_time($audio.currentTime));
+                console.log(psn_time($audio.currentTime))
+
+                var num = Number(currentTime / duration * 100) + "%";
+                $(".jindu-width").css({ width: num })
+
+                if (duration == currentTime) {
+                    $music.m_next();
+                }
+            }, 1000)
+        },
+        m_play() {
+            // 播放
+            $audio.play();
+            clearInterval(iTimer);
+            $music.m_time();
+        },
+        m_pause() {
+            // 暂停
+            $audio.pause();
+            clearInterval(iTimer);
+        },
+        m_click() {
+            mBtn.click(function() {
+                if (audioBtn) {
+                    $music.m_pause();
+                    $(this).addClass('player-pause');
+                    audioBtn = false;
+                } else {
+                    $music.m_play();
+                    $(this).removeClass('player-pause');
+                    audioBtn = true;
+                }
+            });
+        },
+        m_prev() {
+            num--;
+            if (num < 0) {
+                num = 2;
+            }
+            $audio.src = 'music/' + arr[num] + '.mp3';
+        },
+        m_prev_click() {
+            prev.click(function() {
+                $music.m_prev();
+            })
+        },
+        m_next() {
+            num++;
+            if (num > 2) {
+                num = 0;
+            }
+            $audio.src = 'music/' + arr[num] + '.mp3';
+            $audio.play();
+            console.log(arr[num])
+        },
+        m_next_click() {
+            next.click(function() {
+                $music.m_next();
+            })
+        },
+        m_time_jd() {
+            jinduDiv.click(function() {
+                var width = $(this).width();
+                var l = $(this).offset().left;
+                var e = event || window.event;
+                var scrollX = document.documentElement.scrollLeft || document.body.scrollLeft;
+                var x = e.pageX || e.clientX + scrollX;
+
+                var num = Number((x - l) / width * 100) + "%";
+                $(".jindu-width").css({ width: num })
+
+                $audio.currentTime = Number((x - l) / width) * duration;
+                currentTime = $audio.currentTime;
+                currentTimeTxt.html(psn_time(currentTime));
+
+                $music.m_time();
+
+                console.log(currentTime)
+            });
+        },
+        m_volume() {
+            volumeA.click(function() {
+                if (volumeBtn) {
+                    $audio.volume = 0;
+                    volumeBtn = false;
+                    $(this).addClass('volume-none');
+                    $("#volume-jd-width").css({ width: 0 })
+                } else {
+                    $audio.volume = volume;
+                    volumeBtn = true;
+                    $(this).removeClass('volume-none');
+
+                    var num = Number(volume * 100) + "%";
+                    $("#volume-jd-width").css({ width: num })
+                }
+            });
+        },
+        m_volume_jd() {
+            volumeDiv.click(function(event) {
+                var width = $(this).width();
+                var l = $(this).offset().left;
+                var e = event || window.event;
+                var scrollX = document.documentElement.scrollLeft || document.body.scrollLeft;
+                var x = e.pageX || e.clientX + scrollX;
+
+                var num = Number((x - l) / width * 100) + "%";
+                $("#volume-jd-width").css({ width: num })
+
+                $audio.volume = parseFloat((x - l) / width).toFixed(1);
+                volume = $audio.volume;
+
+                console.log(volume)
+            });
+        }
+    }
+    $music.init();
+
+    function psn_time(m) {
+        var hours = Math.floor(m / 3600);
+        var minutes = Math.floor((m % 3600) / 60);
+        var seconds = Math.floor(m % 60);
+
+        if (hours < 10) {
+            hours = "0" + hours;
+        } else {
+            hours = hours;
+        }
+
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        } else {
+            minutes = minutes;
+        }
+
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        } else {
+            seconds = seconds;
+        }
+
+        return hours + ":" + minutes + ":" + seconds;
+    }
+
+})
